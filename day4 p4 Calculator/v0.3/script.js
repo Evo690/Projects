1299d44b859e02ee264ba4a3a2b0c6d8a5cf06bd
Harshit Dagar <159629217+Evo690@users.noreply.github.com>
2024-06-21 21:17:56 +0530

    Add files via upload

---
+++ day4 p4 Calculator/v0.3/script.js
@@ -0,0 +1,25 @@
+function sin(a) {
+    let b = a*(Math.PI/180)
+    let c = Math.sin(b)
+    return c
+    
+}  
+  function cos(a) {
+    let b = a*(Math.PI/180)
+    let c = Math.cos(b)
+    return c
+    
+}
+function tan(a) {
+    let b = a*(Math.PI/180)
+    let c = Math.sin(b)/Math.cos(b)
+    return c
+    
+}
+function log(a,b) {
+    let c = Math.log(a)
+    let d = Math.log(b)
+    let result = c/d
+    return result
+
+}
